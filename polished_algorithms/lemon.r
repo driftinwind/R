bf1e2778900f9979adb1d4fe5715b2220ceca5cb
Milktrader <milktrader@gmail.com>
2011-04-21 22:12:01 -0400

    new files

--- polished_algorithms/lemon.r
+++ polished_algorithms/lemon.r
@@ -1,22 +1,22 @@
 lemon <- function(sym="SPY", expiry=24, pct=.03){
-	
-require("quantmod")                           
-                                              
-ticker <- getSymbols(sym, auto.assign=FALSE)                             
-                                              
-ticker <- ticker[,4]                             
-                                              
-lose <- 0                                      
-                                                                            
-for(i in expiry:NROW(ticker))                                                                                
-                                                    
-if(as.numeric(ticker[i]) < (as.numeric(ticker[i-(expiry-1)]) - as.numeric(ticker[i-(expiry-1)])*pct) )                                            
-                                                                            
-lose <- lose + 1                                                              
-	                                                                        
-losers <- lose/(NROW(ticker)-expiry)	                 
-	                                                                                                
-cat(   " Trading Days:               ", NROW(ticker)-expiry, "\n",
-       "Losers:                    ", lose, "\n",
-       "Percentage of losing days: ", round(losers*100, digits=2), "\n")                                   
-}                                                 
+	                                                                                                   
+require("quantmod")                                                                                    
+                                                                                                       
+ticker <- getSymbols(sym, auto.assign=FALSE)                                                           
+                                                                                                       
+ticker <- ticker[,4]                                                                                   
+                                                                                                       
+lose <- 0                                                                                              
+                                                                                                       
+for(i in expiry:NROW(ticker))                                                                          
+                                                                                                       
+if(as.numeric(ticker[i]) < (as.numeric(ticker[i-(expiry-1)]) - as.numeric(ticker[i-(expiry-1)])*pct) )                       
+                                                                                                       
+lose <- lose + 1                                                                                       
+	                                                                                                   
+losers <- lose/(NROW(ticker)-expiry)	                                                               
+	                                                                                                   
+cat(   " Trading Days:               ", NROW(ticker)-expiry, "\n",                                     
+       "Losers:                    ", lose, "\n",                                                      
+       "Percentage of losing days: ", round(losers*100, digits=2), "\n")                               
+}                                                                                                      
